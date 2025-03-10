2df3249c9da25f56b95a069233e5a5b4ef79f0dd
imbhj <wnqudgus5565@gmail.com>
2025-03-11 07:28:42 +0900

    fix: URL 변경

--- model/counter.model.js
+++ model/counter.model.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define("counter", {
-        count: {
+        value: {
             type: DataTypes.INTEGER,
             allowNull: false
         }
